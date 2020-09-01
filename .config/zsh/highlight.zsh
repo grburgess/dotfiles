e08b1787903a562d1c32757b28ef53a40c360bd6
grburgess <jmichaelburgess@gmail.com>
2020-09-01 18:00:05 +0200

    updates

--- .config/zsh/highlight.zsh
+++ .config/zsh/highlight.zsh
@@ -2,7 +2,7 @@
 typeset -A ZSH_HIGHLIGHT_STYLES
 
 # To differentiate aliases from other command types
-ZSH_HIGHLIGHT_STYLES[alias]='fg=magenta,bold'
+ZSH_HIGHLIGHT_STYLES[alias]='fg =magenta,bold'
 
 # To have paths colored instead of underlined
 ZSH_HIGHLIGHT_STYLES[path]='fg=cyan'
