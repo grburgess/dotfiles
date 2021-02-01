313066c22a73340d767855f2988ae2c1f4b5517f
grburgess <jmichaelburgess@gmail.com>
2021-02-01 07:14:11 +0100

    moving and getting ready for stow

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # editor and terminal
-mkdir -p ~/.config
-mkdir -p ~/.emacs.d/
+mkdir -p ~/.config/emacs/
+
 
 
 ln -s ~/dotfiles/.zshenv ~/.zshenv
@@ -10,37 +10,38 @@ ln -s ~/dotfiles/.config/zsh ~/.config/zsh
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.config/bat ~/.config/bat
 
-ln -s ~/dotfiles/.config/emacs/init.org ~/.emacs.d/init.org 
-cp ~/dotfiles/extra/init.el ~/.emacs.d/init.el
+ln -s ~/dotfiles/.config/emacs/init.org ~/.config/emacs.d/init.org 
+cp ~/dotfiles/extra/init.el ~/.config/emacs/init.el
 
 # get a modern terminal color going
 cp ~/dotfiles/terminfo-24bit.src ~/terminfo-24bit.src
 tic -x -o ~/.terminfo terminfo-24bit.src
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+
+    # install basic brew packages
+    brew install openssl coreutils the fuck
+    brew install git tmux zsh starship
+    brew install exa bat rg fd
+    brew install glances jq fx htop httpie tree gpg graphviz
+    brew install rsync
+    brew install open-mpi
+
+#brew install emacs-plus@27 --with-xwidgets --with-no-frame-refocus --with-no-titlebar --with-retro-sink-bw-icon
+
+    brew install python
+
+    pip install virtualenvwrapper ipython
+
+    mkdir -p ~/.environs
 
 
-# install basic brew packages
-brew install openssl coreutils
-brew install git tmux zsh starship
-brew install exa bat rg fd
-brew install glances jq fx htop httpie tree gpg graphviz
-brew install rsync
-brew install open-mpi
+    VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
+    export WORKON_HOME=~/.environs
 
-brew install emacs-plus@27 --with-xwidgets --with-no-frame-refocus --with-no-titlebar --with-retro-sink-bw-icon
-
-brew install python
-
-pip install virtualenvwrapper ipython
-
-mkdir -p ~/.environs
+    source /usr/local/bin/virtualenvwrapper.sh
 
 
-VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
-export WORKON_HOME=~/.environs
-
-source /usr/local/bin/virtualenvwrapper.sh
-
-
-cp ~/dotfiles/.environs/reqs.txt ~/.environs/reqs.txt
-cp ~/dotfiles/.environs/postmkvirtualenv ~/.environs/postmkvirtualenv 
+    cp ~/dotfiles/.environs/reqs.txt ~/.environs/reqs.txt
+    cp ~/dotfiles/.environs/postmkvirtualenv ~/.environs/postmkvirtualenv 
+fi
