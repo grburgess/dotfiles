ef018e30a5ae9997bd6ceb59058d21c71d74b64c
J. Michael Burgess <jmichaelburgess@gmail.com>
2023-05-21 08:16:32 +0200

    Update .config/emacs/init.el Update .config/emacs/init.org

--- dot_config/emacs/init.el
+++ dot_config/emacs/init.el
@@ -168,6 +168,14 @@ the buffer is buried."
   (sublimity-mode 1))
                                         ;  (sublimity-map-set-delay 3))
 
+(use-package nerd-icons
+  ;; :custom
+  ;; The Nerd Font you want to use in GUI
+  ;; "Symbols Nerd Font Mono" is the default and is recommended
+  ;; but you can use any other Nerd Font if you want
+  ;; (nerd-icons-font-family "Symbols Nerd Font Mono")
+  )
+
 (use-package s)
 (use-package all-the-icons
   :if (display-graphic-p)
